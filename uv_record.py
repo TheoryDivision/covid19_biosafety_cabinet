941f875a0ac4a59f019edfc0d0ae504a7a3de5c5
Vishhvaan Gopalakrishnan <vish0288@gmail.com>
2020-03-26 00:01:05 -0400

    code cleanup

--- uv_record.py
+++ uv_record.py
@@ -2,32 +2,19 @@
 """ Vishhvaan's Test Script """
 
 import time
-# import datetime
 import csv
-# import threading
 import os
-#mport RPi.GPIO as GPIO
 
 import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-#import numpy as np
 
-P_LED_pins = [21]
-#PIO.setmode(GPIO.BCM)
-#PIO.setup(P_LED_pins, GPIO.OUT)
-
-#PIO.output(P_LED_pins,1)
-
-# Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
 
-# Create single-ended input on channel 0
-# photoreceptor_channel = 0
 pd = AnalogIn(ads, ADS.P0)
 pd1 = AnalogIn(ads, ADS.P1)
 pd2 = AnalogIn(ads, ADS.P2)
